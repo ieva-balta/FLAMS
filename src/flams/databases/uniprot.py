91fc1205f05636a42d676cd3460f36ea24319dc6
ieva-balta <ieva.balta@student.kuleuven.be>
2025-10-15 15:53:45 +0200

    Placeholder script for uniprot based on the scripts for other databases

---
+++ src/flams/databases/uniprot.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+""" uniprot
+functions to fetch and parse uniprot entries
+based on scripts for cplm4 and dbptm
+"""
+
+def get_fasta(descriptor, location):
+    """
+    will download uniprot data for each descriptor (modification) seperately
+    and save it in a fasta format
+    return the multi fasta file for the specified decriptor
+    """
+    pass
+
+def _convert_uniprot_to_fasta(uniprot):
+    """
+    will help convert the query output to a fasta format
+    returns sequence and records
+    """
+    pass
