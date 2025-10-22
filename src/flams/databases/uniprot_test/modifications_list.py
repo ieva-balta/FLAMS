4ee9bf539301487799adfbddd942e15d1ba106aa
ieva-balta <ieva.balta@student.kuleuven.be>
2025-10-22 12:20:32 +0200

    updated modification list

--- src/flams/databases/uniprot_test/modifications_list.py
+++ src/flams/databases/uniprot_test/modifications_list.py
@@ -18,7 +18,7 @@ MODIFICATIONS = {
     #     [ModificationDatabase(uniprot), "ft_mod_res:acetyl*"], #acetyl*
     #     ["A","C","D","E","G","K","M","P","R","S","T","V","Y"]
     # ), # works both with keywrod and ft_mod_res
-    "adp-ribosylation": "ft_mod_res:ADP-ribosyl",
+    "adp-ribosylation": "ft_mod_res:ADP",
     #     ModificationType(
     #     "adp-ribosylation", 1.4,
     #     #[ModificationDatabase(dbptm, "ADP-ribosylation")],
@@ -67,7 +67,7 @@ MODIFICATIONS = {
         # [ModificationDatabase(uniprot), "ft_mod_res:Blocked amino end"],  #blocked amino end
         # ["A","C","D","E","G","H","I","L","M","N","P","Q","R","S","T","V"]
     # ), #"Blocked amino end" works
-    "butyrylation": "ft_mod_res:butyryl*",
+    "butyrylation": "ft_mod_res:butyryl*", # includes the beta-hydroxybutyrylation
     #     ModificationType(
     #     "butyrylation", 1.4,
     #     #[ModificationDatabase(cplmv4, "Butyrylation"), ModificationDatabase(dbptm, "Butyrylation")],
@@ -177,7 +177,7 @@ MODIFICATIONS = {
     #     [ModificationDatabase(dbptm, "Farnesylation")],
     #     ["C"]
     # ),
-    "formation_of_an_isopeptide_bond": "ft_crosslnk:isopeptide",
+    "formation_of_an_isopeptide_bond": "ft_crosslnk:isopeptide", # lots of overlap with the sumo, ubiq, nedd
     #ModificationType(
     #     "formation_of_an_isopeptide_bond", 1.4,
     #     [ModificationDatabase(dbptm, "Formation of an isopeptide bond")],
@@ -213,242 +213,293 @@ MODIFICATIONS = {
     #     [ModificationDatabase(dbptm, "Glutathionylation")],
     #     ["C"]
     # ),
-    # "glycation": ModificationType(
+    "glycation": "ft_carbohyd:glycation",
+    #ModificationType(
     #     "glycation", 1.4, [ModificationDatabase(cplmv4, "Glycation")],
     #     ["K"]
     # ),
-    # "gpi-anchor": ModificationType(
+    "gpi-anchor": "ft_lipid:gpi-anchor",
+    #ModificationType(
     #     "gpi-anchor", 1.4, [ModificationDatabase(dbptm, "GPI-anchor")],
     #     ["A","C","D","G","N","S","T"]
     # ),
-    # "hmgylation": ModificationType(
-    #     "hmgylation", 1.4, [ModificationDatabase(cplmv4, "HMGylation")],
-    #     ["K"] #OK
-    # ),
-    # "hydroxyceramide_ester": ModificationType(
+    # "hmgylation": 
+    # #ModificationType(
+    # #     "hmgylation", 1.4, [ModificationDatabase(cplmv4, "HMGylation")],
+    # #     ["K"] #OK
+    # # ),
+    "hydroxyceramide_ester": "ft_lipid:hydroxyceramide glutamate ester",
+    # ModificationType(
     #     "hydroxyceramide_ester", 1.4, [ModificationDatabase(dbptm, "Hydroxyceramide ester")],
     #     ["Q"]
     # ),
-    # "hydroxylation": ModificationType(
+    "hydroxylation": "ft_mod_res:hydroxy*",
+    # ModificationType(
     #     "hydroxylation", 1.4,
     #     [ModificationDatabase(cplmv4, "Hydroxylation"), ModificationDatabase(dbptm, "Hydroxylation")],
     #     ["C","D","E","F","H","I","K","L","N","P","R","S","T","V","W","Y"]
     # ),
-    # "iodination": ModificationType(
+    "iodination": "ft_mod_res:iodotyrosine",
+    # ModificationType(
     #     "iodination", 1.4,
     #     [ModificationDatabase(dbptm, "Iodination")],
     #     ["Y"]
     # ),
-    # "lactoylation": ModificationType(
+    "lactoylation": "ft_mod_res:lactoyllysine",
+    # ModificationType(
     #     "lactoylation", 1.4,
     #     [ModificationDatabase(dbptm, "Lactoylation")],
     #     ["K"]
     # ),
-    # "lactylation": ModificationType(
-    #     "lactylation", 1.4,
-    #     [ModificationDatabase(cplmv4, "Lactylation"), ModificationDatabase(dbptm, "Lactylation")],
-    #     ["K"]
-    # ),
-    # "lipoylation": ModificationType(
+    # "lactylation": "",
+    # # ModificationType(
+    # #     "lactylation", 1.4,
+    # #     [ModificationDatabase(cplmv4, "Lactylation"), ModificationDatabase(dbptm, "Lactylation")],
+    # #     ["K"]
+    # # ),
+    "lipoylation": "ft_mod_res:lipoyllysine",
+    # ModificationType(
     #     "lipoylation", 1.4,
     #     [ModificationDatabase(cplmv4, "Lipoylation"), ModificationDatabase(dbptm, "Lipoylation")],
     #     ["K"]
     # ),
-    # "malonylation": ModificationType(
+    "malonylation": "ft_mod_res:malonyl*",
+    # ModificationType(
     #     "malonylation", 1.4,
     #     [ModificationDatabase(cplmv4, "Malonylation"), ModificationDatabase(dbptm, "Malonylation")],
     #     ["K"]
     # ),
-    # "methylation": ModificationType(
+    "methylation": "ft_mod_res:methyl*",
+    # ModificationType(
     #     "methylation", 1.4,
     #     [ModificationDatabase(cplmv4, "Methylation"), ModificationDatabase(dbptm, "Methylation")],
     #     ["A","C","D","E","F","G","H","I","K","L","M","N","P","Q","R","S","T","V","Y"]
     # ),
-    # "mgcylation": ModificationType(
-    #     "mgcylation", 1.4, [ModificationDatabase(cplmv4, "MGcylation")],
-    #     ["K"]
-    # ),
-    # "mgylation": ModificationType(
-    #     "mgylation", 1.4, [ModificationDatabase(cplmv4, "MGylation")],
-    #     ["K"]
-    # ),
-    # "myristoylation": ModificationType(
+    # "mgcylation": "",
+    # # ModificationType(
+    # #     "mgcylation", 1.4, [ModificationDatabase(cplmv4, "MGcylation")],
+    # #     ["K"]
+    # # ),
+    # "mgylation": "",
+    # # ModificationType(
+    # #     "mgylation", 1.4, [ModificationDatabase(cplmv4, "MGylation")],
+    # #     ["K"]
+    # # ),
+    "myristoylation": "ft_lipid:myristoyl*",
+    # ModificationType(
     #     "myristoylation", 1.4, [ModificationDatabase(dbptm, "Myristoylation")],
     #     ["C","G","K"]
     # ),
-    # "neddylation": ModificationType(
+    "neddylation": "ft_crosslnk:NEDD8", # might not be complete
+    # ModificationType(
     #     "neddylation", 1.4,
     #     [ModificationDatabase(cplmv4, "Neddylation"), ModificationDatabase(dbptm, "Neddylation")],
     #     ["K"]
     # ),
-    # "nitration": ModificationType(
+    "nitration": "ft_mod_res:nitrotyrosine",
+    # ModificationType(
     #     "nitration", 1.4, [ModificationDatabase(dbptm, "Nitration")],
     #     ["Y"]
     # ),
-    # "n-carbamoylation": ModificationType(
+    "n-carbamoylation": "ft_mod_res:N-carbamoylalanine",
+    # ModificationType(
     #     "n-carbamoylation", 1.4, [ModificationDatabase(dbptm, "N-carbamoylation")],
     #     ["A"]
     # ),
-    # "n-linked_glycosylation": ModificationType(
+    "n-linked_glycosylation": "ft_carbohyd:n-linked",
+    # ModificationType(
     #     "n-linked_glycosylation", 1.4,
     #     [ModificationDatabase(dbptm, "N-linked Glycosylation")],
     #     ["D","I","K","N","R","S","T","V","W"]
     # ),
-    # "n-palmitoylation": ModificationType(
+    "n-palmitoylation": "ft_lipid:n-palmitoyl",
+    # ModificationType(
     #     "n-palmitoylation", 1.4, [ModificationDatabase(dbptm, "N-palmitoylation")],
     #     ["C","G","K"]
     # ),
-    # "octanoylation": ModificationType(
+    "octanoylation": "ft_lipid:octanoyl",
+    # ModificationType(
     #     "octanoylation", 1.4,
     #     [ModificationDatabase(dbptm, "Octanoylation")],
     #     ["S","T"]
     # ),
-    # "oxidation": ModificationType(
-    #     "oxidation", 1.4,
-    #     [ModificationDatabase(dbptm, "Oxidation")],
-    #     ["C","L","M","S","W"]
-    # ),
-    # "o-linked_glycosylation": ModificationType(
+    # "oxidation": "", #Tryptophylquinone? Methionine sulfoxide? Cysteine sulfenic acid?
+    # # ModificationType(
+    # #     "oxidation", 1.4,
+    # #     [ModificationDatabase(dbptm, "Oxidation")],
+    # #     ["C","L","M","S","W"]
+    # # ),
+    "o-linked_glycosylation": "ft_carbohyd:o-linked",
+    # ModificationType(
     #     "o-linked_glycosylation", 1.4,
     #     [ModificationDatabase(dbptm, "O-linked Glycosylation")],
     #     ["K","P","S","T","Y"]
     # ),
-    # "o-palmitoleoylation": ModificationType(
+    "o-palmitoleoylation": "ft_lipid:o-palmitoleoyl",
+    # ModificationType(
     #     "o-palmitoleoylation", 1.4,
     #     [ModificationDatabase(dbptm, "O-palmitoleoylation")],
     #     ["S"]
     # ),
-    # "o-palmitoylation": ModificationType(
+    "o-palmitoylation": "ft_lipid:o-palmitoyl",
+    # ModificationType(
     #     "o-palmitoylation", 1.4,
     #     [ModificationDatabase(dbptm, "O-palmitoylation")],
     #     ["S","T"]
     # ),
-    # "phosphatidylethanolamine_amidation": ModificationType(
+    "phosphatidylethanolamine_amidation": "ft_lipid:phosphatidylethanolamine amidated",
+    # ModificationType(
     #     "phosphatidylethanolamine_amidation", 1.4, [ModificationDatabase(dbptm, "Phosphatidylethanolamine amidation")],
     #     ["G"]
     # ),
-    # "phosphoglycerylation": ModificationType(
-    #     "phosphoglycerylation", 1.4,
-    #     [ModificationDatabase(cplmv4, "Phosphoglycerylation")],
-    #     ["K"]
-    # ),
-    # "phosphorylation": ModificationType(
+    # "phosphoglycerylation": "";
+    # # ModificationType(
+    # #     "phosphoglycerylation", 1.4,
+    # #     [ModificationDatabase(cplmv4, "Phosphoglycerylation")],
+    # #     ["K"]
+    # # ),
+    "phosphorylation": "ft_mod_res:phospho*",
+    # ModificationType(
     #     "phosphorylation", 1.4,
     #     [ModificationDatabase(dbptm, "Phosphorylation")],
     #     ["A","C","D","E","F","G","H","I","K","L","N","P","Q","R","S","T","V","W","Y"]
     # ),
-    # "propionylation": ModificationType(
+    "propionylation": "ft_mod_res:propionyl*",
+    # ModificationType(
     #     "propionylation", 1.4,
     #     [ModificationDatabase(cplmv4, "Propionylation"), ModificationDatabase(dbptm, "Propionylation")],
     #     ["K"]
     # ),
-    # "pupylation": ModificationType(
+    "pupylation": "ft_crosslnk:Pup",
+    # ModificationType(
     #     "pupylation", 1.4, [ModificationDatabase(cplmv4, "Pupylation")],
     #     ["K"]
     # ),
-    # "pyrrolidone_carboxylic_acid": ModificationType(
+    "pyrrolidone_carboxylic_acid": "ft_mod_res:pyrrolidone carboxylic acid",
+    # ModificationType(
     #     "pyrrolidone_carboxylic_acid", 1.4, [ModificationDatabase(dbptm, "Pyrrolidone carboxylic acid")],
     #     ["E","Q"]
     # ),
-    # "pyrrolylation": ModificationType(
+    "pyrrolylation": "ft_mod_res:*pyrrolyl*",
+    # ModificationType(
     #     "pyrrolylation", 1.4, [ModificationDatabase(dbptm, "Pyrrolylation")],
     #     ["C"]
     # ),
-    # "pyruvate": ModificationType(
+    "pyruvate": "ft_mod_res:pyruvic acid",
+    # ModificationType(
     #     "pyruvate", 1.4, [ModificationDatabase(dbptm, "Pyruvate")],
     #     ["C","S"]
     # ),
-    # "serotonylation" : ModificationType(
+    "serotonylation" : "ft_mod_res:serotonin",
+    # ModificationType(
     #     "serotonylation", 1.4,
     #     [ModificationDatabase(dbptm, "Serotonylation")],
     #     ["Q"]
     # ),
-    # "stearoylation" : ModificationType(
+    "stearoylation" : "ft_lipid:stearoyl",
+    # ModificationType(
     #     "stearoylation", 1.4,
     #     [ModificationDatabase(dbptm, "Stearoylation")],
     #     ["C"]
     # ),
-    # "succinylation": ModificationType(
+    "succinylation": "succinyl*",
+    # ModificationType(
     #     "succinylation", 1.4,
     #     [ModificationDatabase(cplmv4, "Succinylation"), ModificationDatabase(dbptm, "Succinylation")],
     #     ["C","K","W"]
     # ),
-    # "sulfation" : ModificationType(
+    "sulfation" : "ft_mod_res:sulfo*",
+    # ModificationType(
     #     "sulfation", 1.4,
     #     [ModificationDatabase(dbptm, "Sulfation")],
     #     ["C","S","T","Y"]
     # ),
-    # "sulfhydration" : ModificationType(
+    "sulfhydration" : "ft_mod_res:cysteine persulfide",
+    # ModificationType(
     #     "sulfhydration", 1.4,
     #     [ModificationDatabase(dbptm, "Sulfhydration")],
     #     ["C"]
     # ),
-    # "sulfoxidation" : ModificationType(
+    "sulfoxidation" : "ft_mod_res:sulfoxide",
+    # ModificationType(
     #     "sulfoxidation", 1.4,
     #     [ModificationDatabase(dbptm, "Sulfoxidation")],
     #     ["M"]
     # ),
-    # "sumoylation": ModificationType(
+    "sumoylation": "ft_crosslnk:SUMO", # how to query the sumoylated lysine???
+    # ModificationType(
     #     "sumoylation", 1.4,
     #     [ModificationDatabase(cplmv4, "Sumoylation"), ModificationDatabase(dbptm, "Sumoylation")],
     #     ["K"]
     # ),
-    # "s-archaeol" : ModificationType(
+    "s-archaeol" : "ft_lipid:s-archaeol",
+    # ModificationType(
     #     "s-archaeol", 1.4,
     #     [ModificationDatabase(dbptm, "S-archaeol")],
     #     ["C"]
     # ),
-    # "s-carbamoylation" : ModificationType(
+    "s-carbamoylation" : "ft_mod_res:s-carbamoylcysteine",
+    # ModificationType(
     #     "s-carbamoylation", 1.4,
     #     [ModificationDatabase(dbptm, "S-carbamoylation")],
     #     ["C"]
     # ),
-    # "s-cyanation" : ModificationType(
+    "s-cyanation" : "ft_mod_res:s-cyanocysteine",
+    # ModificationType(
     #     "s-cyanation", 1.4,
     #     [ModificationDatabase(dbptm, "S-Cyanation")],
     #     ["C"]
     # ),
-    # "s-cysteinylation" : ModificationType(
+    "s-cysteinylation" : "ft_mod_res:s-cysteinyl",
+    # ModificationType(
     #     "s-cysteinylation", 1.4,
     #     [ModificationDatabase(dbptm, "S-cysteinylation")],
     #     ["C"]
     # ),
-    # "s-diacylglycerol" : ModificationType(
+    "s-diacylglycerol" : "ft_lipid:s-diacylglycerol",
+    # ModificationType(
     #     "s-diacylglycerol", 1.4,
     #     [ModificationDatabase(dbptm, "S-diacylglycerol")],
     #     ["C"]
     # ),
-    # "s-linked_glycosylation": ModificationType(
+    "s-linked_glycosylation": "ft_carbohyd:s-linked",
+    # ModificationType(
     #     "s-linked_glycosylation", 1.4,
     #     [ModificationDatabase(dbptm, "S-linked Glycosylation")],
     #     ["C"]
     # ),
-    # "s-nitrosylation" : ModificationType(
+    "s-nitrosylation" : "ft_mod_res:s-nitrosocysteine", # overlaps with nitro*?
+    # ModificationType(
     #     "s-nitrosylation", 1.4,
     #     [ModificationDatabase(dbptm, "S-nitrosylation")],
     #     ["C"]
     # ),
-    # "s-palmitoylation" : ModificationType(
+    "s-palmitoylation" : "ft_lipid:s-palmitoyl",
+    # ModificationType(
     #     "s-palmitoylation", 1.4,
     #     [ModificationDatabase(dbptm, "S-palmitoylation")],
     #     ["C"]
     # ),
-    # "thiocarboxylation" : ModificationType(
+    "thiocarboxylation" : "ft_mod_res:thioglycine",
+    # ModificationType(
     #     "thiocarboxylation", 1.4,
     #     [ModificationDatabase(dbptm, "Thiocarboxylation")],
     #     ["G"]
     # ),
-    # "ubiquitination": ModificationType(
-    #     "ubiquitination", 1.4,
-    #     [ModificationDatabase(cplmv4, "Ubiquitination"), ModificationDatabase(dbptm, "Ubiquitination")],
-    #     ["C","K","R","S"]
-    # ),
-    # "umpylation" : ModificationType(
+    "ubiquitination": "ft_crosslnk:ubiquitin", # same problem as sumoylation
+    # # ModificationType(
+    # #     "ubiquitination", 1.4,
+    # #     [ModificationDatabase(cplmv4, "Ubiquitination"), ModificationDatabase(dbptm, "Ubiquitination")],
+    # #     ["C","K","R","S"]
+    # # ),
+    "umpylation" : "ft_mod_res:UMP",
+    # ModificationType(
     #     "umpylation", 1.4,
     #     [ModificationDatabase(dbptm, "UMPylation")],
     #     ["S","T","Y"]
     # ),
-    # "2-hydroxyisobutyrylation": ModificationType(
+    "2-hydroxyisobutyrylation": "ft_mod_res:2-hydroxyisobutyryl",
+    # ModificationType(
     #     "2-hydroxyisobutyrylation", 1.4, [ModificationDatabase(cplmv4, "2-Hydroxyisobutyrylation")],
     #     ["K"]
     # ),
@@ -456,4 +507,5 @@ MODIFICATIONS = {
 
 # code to run through the list and create the databases
 for m, desc in MODIFICATIONS.items():
-    get_fasta_rest(desc, f"./dbs/{m}.fasta")
+    #get_fasta_rest(desc, f"./dbs/{m}.fasta")
+    get_fasta_rest(desc, f"/scratch/leuven/368/vsc36826/IBP/run2/{m}.fasta")
